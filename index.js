65fd97366a38e8169657920dcdd00efa19aa2d92
ballpanuwat25 <ballpanuwat25@gmail.com>
2025-01-17 21:22:06 +0700

    add eurusd

--- index.js
+++ index.js
@@ -27,4 +27,27 @@ $(document).ready(function() {
         let result = risk_amount / (pip * pipvalue);
         $('#lotsize').text(result.toFixed(2));
     });
+
+    $('#eu_stoploss').on('input', function() {
+        let stoploss = $(this).val();
+
+        if(stoploss == 0) {
+            $('#eu_lotsize').text('0.00');
+            return;
+        }
+
+        let result = risk_amount / (stoploss * pipvalue);
+        $('#eu_lotsize').text(result.toFixed(2));
+    });
+
+    $('#eu_calculate').on('click', function() {
+        let openprice = $('#eu_openprice').val();
+        let stoploss = $('#eu_stoploss_price').val();
+        
+        let pip = openprice - stoploss;
+        pip = Math.abs(pip) * 100000;
+
+        let result = risk_amount / (pip * pipvalue);
+        $('#eu_lotsize').text(result.toFixed(2));
+    });
 });
